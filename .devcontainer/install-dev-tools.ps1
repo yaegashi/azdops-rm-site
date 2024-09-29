09d7f270402bfc00a9404e2357e4f59a2a4265f7
YAEGASHI Takeshi <yaegashi@gmail.com>
2024-09-29 17:00:57 +0000

    Add .devcontainer

---
+++ .devcontainer/install-dev-tools.ps1
@@ -0,0 +1,5 @@
+$ProgressPreference = 'SilentlyContinue'
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name Microsoft.Graph.Authentication
+Install-Module -Name Microsoft.Graph.Users
+Install-Module -Name Microsoft.Graph.Groups
