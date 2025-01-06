56e8ac994ad69d18facabbbe3c28745280fea4ee
YAEGASHI Takeshi <yaegashi@gmail.com>
2025-01-06 09:54:51 +0000

    Dev Container Prebuild

--- .devcontainer/install-dev-tools.ps1
+++
@@ -1,6 +0,0 @@
-$ProgressPreference = 'SilentlyContinue'
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name Microsoft.Graph.Authentication
-Install-Module -Name Microsoft.Graph.Users
-Install-Module -Name Microsoft.Graph.Groups
-Install-Module -Name powershell-yaml
--- .devcontainer/install-dev-tools.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends \
-    bash-completion \
-    default-mysql-client-core \
-    postgresql-client \
-    sqlite3
